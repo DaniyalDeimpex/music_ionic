61e270713d19223e8691ebb418ac3cd72fef4560
Daniyal Saleem <daniyal@deimpex.com>
2018-11-29 17:39:32 -0800

    message

---
+++ src/providers/musics/musics.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const API: string = "https://raw.githubusercontent.com/CsgeeksYoutube/ionic-music-app-data-file/master/New%20Text%20Document.json";
+@Injectable()
+export class MusicsProvider {
+  constructor(public http: HttpClient) {
+    console.log('Hello MusicsProvider Provider');
+  }
+
+  getMusic() {
+    return this.http.get<any[]>(API);
+  }
+
+}
